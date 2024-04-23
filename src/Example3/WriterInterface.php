c486f139108475a33c16e781a5272446a46cb73c
Jørn Fauske <jorn@ramsalt.com>
2024-04-23 19:47:32 +0200

    Update interface for setter injection example

--- src/Example3/WriterInterface.php
+++ src/Example3/WriterInterface.php
@@ -6,5 +6,6 @@ use Phpbergen\Di\Example3\FormatterInterface;
 
 interface WriterInterface
 {
+    public function setFormatter(FormatterInterface $formatter): void;
     public function write(string $message): void;
 }
